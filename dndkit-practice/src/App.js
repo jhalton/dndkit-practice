05ad59b737d1d3d74ac64def01a73e68defb4b24
Jeanette <haltonjeanette@gmail.com>
2025-05-22 17:03:22 -0700

    Stopped to frost cupcakes. Built out basic Droppable and Draggable components, but now I need to put them to work

--- dndkit-practice/src/App.js
+++ dndkit-practice/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { DndContext } from "@dnd-kit/core";
+
+import Draggable from "./Components/Draggable";
+import Droppable from "./Components/Droppable";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DndContext>
+        <Draggable />
+        <Droppable />
+      </DndContext>
     </div>
   );
 }
